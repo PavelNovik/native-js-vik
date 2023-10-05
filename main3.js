89a6409e65bb7c117bb93f684b8fded2ca582a74
Pavel Novik <brn2befree@gmail.com>
2023-10-05 14:21:44 +0300

    Associative array

---
+++ main3.js
@@ -0,0 +1,32 @@
+const user = {
+    name: "Pavel",
+    age: 37,
+    address: {
+        city: {
+            title: "Sluck",
+        }
+    }
+}
+const user1 = {
+    "name": "Pavel",
+    "age": 37,
+    "address": {
+        "city": {
+            "title": "Sluck",
+        }
+    }
+}
+const users = ['Dimych', 'Natasha', 'Valera', 'Katya']
+const usersObj = {
+    0: 'Dimych',
+    1: "Natasha",
+    2: "Valera",
+    3: "Katya",
+}
+
+console.log(usersObj[2])
+
+console.log(user.address.city.title)
+console.log(user["address"]["city"]["title"])
+console.log(user1.address.city.title)
+console.log(user1["address"]["city"]["title"])
